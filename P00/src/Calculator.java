4e9a45acd7d2e5d0986b6e1c571b58ee0bb9d321
diya1256 <22000028@myrp.edu.sg>
2023-05-29 13:29:57 +0800

    First push to P00 repository in GitHub

---
+++ P00/src/Calculator.java
@@ -0,0 +1,16 @@
+
+public class Calculator {
+	public int add(int a , int b) {
+		return a+b;
+	}
+	
+	public int substract(int a, int b) {
+		return a-b;
+	}
+	public int multiply(int a, int b) {
+		return a*b;
+	}
+	public int devide (int a, int b) {
+		 return a / b;
+		}
+}
